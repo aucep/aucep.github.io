5a7d484b9c67e84708a71f220ee2e43c9b5bd812
auceps <henscripting@gmail.com>
2021-04-11 21:11:37 -0400

    gooder

--- mane.js
+++ mane.js
@@ -1,60 +1,135 @@
-'use strict';
+"use strict";
 
-const content = $('content');
-content.load = async function (filename) {
-  content.innerText = 'loading';
-  const resp = await fetch('/written/'+filename);
+const contentPane = $('content-pane');
+contentPane.style.display = '';
+
+const content = $("content");
+content.load = async function (url) {
+  content.innerText = "loading";
+  const resp = await fetch(url);
 
   if (!resp.ok) {
-    this.innerText = 'failed: '+resp.statusText;
+    this.innerText = "failed: " + resp.statusText;
   }
 
   this.raw = await resp.text();
-  content.innerText = '';
-  
-  const page = Parse.parseLang(this.raw, '12y');
+  content.innerText = "";
+
+  const page = Parse.parseLang(this.raw, "12y");
 
   this.appendChild(page);
 };
 
+const chooserPathInput = $("path-input");
+chooserPathInput.value = '';
 
-const chooser = $('chooser');
-chooser.load = async function() {
-  loadButton.disabled = true;
-  this.disabled = true;
-  Array.from(this.children).forEach(o => o.remove());
-  const loading = document.createElement('option');
-  loading.textContent = 'loading';
-  this.appendChild(loading);
+const chooserInfo = $("info");
 
-  const resp = await fetch('https://api.github.com/repos/aucep/written/contents');
-  if (!resp.ok) {
-    this.firstElementChild.textContent = 'could not load';
-    return
+const chooserFiles = $("files");
+chooserFiles.populate = function (files) {
+  if (files.length == 0) this.textContent = "no files";
+  else {
+    while (this.lastChild) this.removeChild(this.lastChild);
+
+    const template = $("file-template");
+
+    files.forEach((f) => {
+      const option = template.content.firstElementChild.cloneNode(true);
+      option.lastElementChild.textContent = f.name;
+      option.path = f.download_url;
+      this.append(option);
+    });
   }
+};
 
-  const files = await resp.json();
-  if (files.length == 0) {
-    this.firstElementChild.textContent = 'no files!';
-    return
-  }
-
-  Array.from(this.children).forEach(o => o.remove());
-  files.forEach(f => {
-    const option = document.createElement('option');
-    option.textContent = option.value = f.name;
-    this.appendChild(option);
-  });
-  this.disabled = false;
-  loadButton.disabled = false;
+function loadFile(e) {
+  const o = e.currentTarget;
+  if (o.classList.contains("chosen")) return;
+  content.load(o.path);
+  Array.from(o.parentNode.children).forEach((o) =>
+    o.classList.remove("chosen")
+  );
+  o.classList.add("chosen");
 }
 
-const loadButton = $('load-button');
-loadButton.onclick = () => content.load(chooser.value);
+const chooserDirs = $("dirs");
+chooserDirs.populate = function (dirs) {
+  if (dirs.length == 0) this.textContent = "no dirs";
+  else {
+    while (this.lastChild) this.removeChild(this.lastChild);
 
-chooser.load();
+    const template = $("dir-template");
 
+    dirs.forEach((d) => {
+      const option = template.content.firstElementChild.cloneNode(true);
+      option.lastElementChild.textContent = d.name;
+      option.path = d.path;
+      this.append(option);
+    });
+  }
+};
 
+function loadDir(e) {
+  const o = e.currentTarget;
+  let path;
+  if (o.tagName == 'BUTTON') {
+    path = chooserPathInput.value ?? '';
+  } else {
+    path = chooserPathInput.value = o.path;
+  }
+  chooser.load(path);
+}
 
-function $(id) { return document.getElementById(id) }
-//function $$(q) { return document.querySelector(q) }
+const openDir = $('open-dir');
+
+const chooser = $("frame");
+chooser.load = async function (path) {
+  chooserInfo.textContent = "loading dir";
+
+  const resp = await fetch(
+    "https://api.github.com/repos/aucep/written/contents/" + path
+  );
+  if (!resp.ok) {
+    chooserInfo.textContent = "could not load dir";
+    return;
+  }
+
+  const dirContents = await resp.json();
+  print(dirContents);
+  const files = dirContents.filter((f) => f.type == "file");
+  chooserFiles.populate(files);
+  const dirs = dirContents.filter((f) => f.type == "dir");
+  chooserDirs.populate(dirs);
+
+  const openDirSpan = openDir.querySelector('span');
+  if (path == "") {
+    if (!openDir.classList.contains('root')) openDir.classList.add('root');
+    openDirSpan.textContent = "(root)";
+  } else {
+    openDir.classList.remove('root');
+    openDirSpan.textContent = path;
+  }
+
+  chooserInfo.textContent = "";
+};
+
+chooser.load('');
+
+function loadParentDir() {
+  let path = openDir.textContent;
+  if (path == "(root)") return;
+  path = path.split('/');
+  chooser.load(path.slice(0,path.length-1));
+}
+
+function print() {
+  console.log(...arguments);
+  return arguments;
+}
+
+function $(id) {
+  return document.getElementById(id);
+}
+function $$(q) {
+  return document.querySelector(q);
+}
